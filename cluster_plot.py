9cf15e9a7e5d6ad00722ec633ef8c113eafbd913
Zan Thomson <zan.thomson@gmail.com>
2019-06-07 15:54:02 +1000

    decoupled cluster & embeddings

--- cluster_plot.py
+++ cluster_plot.py
@@ -2,8 +2,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
 
+import numpy as np
+import pandas as pd
+
 
 def plot_label_dist(cluster,palette='inferno',figsize=(10,5),flip_axes=False):
+    
+    # Graph for dist of merchants across labels
+    
     labels = sorted(cluster.labels_.astype(float))
     Y = [labels.count(x) for x in set(labels)]
     X = list(set(labels))
@@ -15,37 +21,49 @@ def plot_label_dist(cluster,palette='inferno',figsize=(10,5),flip_axes=False):
     plt.ylabel('# of member restaurants')
 
     plt.title('distribution across {} clusters'.format(len(set(labels))))
+    plt.show()
     
 
+
 def plot_clusters(cluster
-                  ,pc
+                  ,pc=None
                   ,text=False
                   ,n_names=10
                   ,centroids=False
                   ,figsize=(6,6)
-                  ,multiple_plots=False):
+                  ,multiple_plots=False
+                  ,labels=[]
+                  ,names=None
+                  ,title=''):
+    
+    # Graph for coloured scatterplot
+    
     sns.set()
     pca0 = pc[:,0]
     pca1 = pc[:,1]
     pca2 = pc[:,2]
     
+    if len(labels)==0:
+        labels = cluster.labels_
+    
     if centroids: centroid_plot = cluster.cluster_centers_
     n = 2 if multiple_plots else 1
+    
     for i,other_pc in enumerate([pca1,pca2][:n]):
         count=0
         
         plt.figure(figsize=figsize)
         plt.scatter(x=pca0,y=other_pc
-                    ,c=kmeans.labels_.astype(float)
+                    ,c=labels.astype(float)
                     , s=50
                     , alpha=0.5)
         if centroids: plt.scatter(centroid_plot[:,0], centroid_plot[:,1], c='blue', s=50)
         
-        #Add merchant name annontations
+        # Add merchant name annontations
         if text:
             for n, (j, x, y) in enumerate(zip(names, pca0, other_pc)):
                 if count>n_names: break
-                if np.random.rand(1)[0]>0.5:
+                if np.random.rand(1)[0]>0.8:
                     count+=1
                     
                     xytexts = [+3,-3,+5,-5,+7,-7]
@@ -54,13 +72,19 @@ def plot_clusters(cluster
                     plt.annotate(j, xy=(x, y),xytext=(x+xco, y+yco),fontsize=10,
                         arrowprops=dict(facecolor=np.random.rand(3), shrink=0.05),)
 
-        plt.title('component0 and component{}'.format(i))
+        plt.title('{}'.format(title))
         plt.show()
 
+# plot for 3d scatterplot, colour coded by label
+
 def plot_3d_clusters(clusters
-                     ,pc
                      ,text=False
-                     ,n_names=8):
+                     ,n_names=8
+                     ,names=None
+                     ,pc=None
+                     ,figsize=(6,6)):
+    
+    from mpl_toolkits.mplot3d import Axes3D
     np.random.seed(5)
 
     X = pc[:,0]
@@ -71,7 +95,7 @@ def plot_3d_clusters(clusters
 
     for name,est in clusters:
         count=0
-        fig = plt.figure(fignum, figsize=(8, 6))
+        fig = plt.figure(fignum, figsize=figsize)
         ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
 
         ax.scatter(X, Y, Z,
@@ -87,8 +111,6 @@ def plot_3d_clusters(clusters
         ax.dist = 12
         fignum = fignum + 1
         
-    
-    
     if text:
         for n, (j, x, y, z) in enumerate(zip(names, X,Y,Z)):
             if count>n_names: break
@@ -101,4 +123,4 @@ def plot_3d_clusters(clusters
                 plt.annotate(j, xy=(x, y),xytext=(x+xco, y+yco),fontsize=10,
                     arrowprops=dict(facecolor=np.random.rand(3), shrink=0.05),)
 
-    fig.show()    
+    fig.show()   
